5756e32ed34680d8985dc4666a1da14718540b61
Daniel Brewer <danielbrewer2014@comcast.net>
2018-12-04 11:20:33 -0600

    first three javascript scope exercises complete

---
+++ main.js
@@ -0,0 +1,34 @@
+const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+for (let x = 0; x < cookies.length; x++) {
+    const currentCookie = cookies[x]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
+
+
+
+const conjunction = function (firstWord, secondWord) {
+    const conjoinedWord = `${firstWord} ${secondWord}`
+    console.log(conjoinedWord)
+}
+
+conjunction("Master", "Card")
+
+
+
+{
+    const ModSquad = {
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+        "series": {
+            "start": "1968",
+            "end": "1973"
+        }
+    }
+
+    let HTMLRepresentation = `<h1>The Mod Squad</h1>`; 
+    ModSquad.members.forEach(member => {
+        HTMLRepresentation += `<li>${member}</li>`;  
+        document.querySelector(".show-info").innerHTML = HTMLRepresentation;
+    })
+}
+
+
