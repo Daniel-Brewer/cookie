aae5c6ea66b5a8775f4a1ac4ccfe6ae3db8fff17
Daniel Brewer <danielbrewer2014@comcast.net>
2018-12-04 11:52:06 -0600

    added Simon says exercise

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+// C is for Cookie 
+
 const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
 for (let x = 0; x < cookies.length; x++) {
     const currentCookie = cookies[x]
     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
 }
 
-
+// Conjuction function
 
 const conjunction = function (firstWord, secondWord) {
     const conjoinedWord = `${firstWord} ${secondWord}`
@@ -13,7 +15,7 @@ const conjunction = function (firstWord, secondWord) {
 
 conjunction("Master", "Card")
 
-
+// Mod Squad
 
 {
     const ModSquad = {
@@ -31,4 +33,20 @@ conjunction("Master", "Card")
     })
 }
 
+// Simon Says
 
+const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+let counter = 0;
+for (let k = 0; k < locations.length; k++) {
+    const currentLocation = locations[k]
+
+    // some of this code seemd redundant so I commented it out
+    if (currentLocation[1] > 2) {
+        // let invalidLocation = true;
+        console.log("This location is invalid")
+        counter += 1;
+        // if (invalidLocation) {
+        // }
+    }
+}
+console.log(`There were ${counter} invalid locations`)
